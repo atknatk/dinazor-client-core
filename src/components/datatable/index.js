9c71cfd8913c38db14be6d1c8916f66d8072ad22
Atakan <atknatk@gmail.com>
2017-10-12 12:00:15 +0300

    remove *.js
    remove *.js

--- src/components/datatable/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./dn-datatable.component"));
-__export(require("./dn-datatable.base"));
-__export(require("./dn-datatable.module"));
-__export(require("./dn-datatable-column.base"));
